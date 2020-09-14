a4c0dd28451497567d362adf68fc3ad9394b66c9
thenamecouldbeLiu <pppoe4016@gmail.com>
2020-09-14 18:52:54 +0800

    0914

--- listo_backend_moduals/config.py
+++ listo_backend_moduals/config.py
@@ -1,10 +1,12 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     #上傳KEY
     SECRET_KEY = os.environ.get('SECRET_KEY') or "LONG_LONG_KEY_HERE" #Flask需要的後台金鑰
     #RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY') or "LONG_LONG_PUBLIC_KEY_HERE" #RECAPTCHA金鑰
     #RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY') or "LONG_LONG_PRIVATE_KEY_HERE"
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://listo_admin:msk60514@localhost:3306/Listo_local' #資料庫連結
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://listo_admin:msk60514@localhost:3306/Listo_local' or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    #資料庫連結
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
